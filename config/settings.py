6752c84b81a9f5426d878b6c500aa6e5294b9d86
kohei-irohasu <yktmd059@icloud.com>
2023-12-24 18:35:06 +0900

    #2

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import environ
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,12 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'users',
     'stories',
     'tags',
     'follows',
     'bookmarks'
 ]
+
+AUTH_USER_MODEL = 'users.Users'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

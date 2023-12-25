1fbc9f925f5580970dcdb3ad30b280158f3647c4
kohei-irohasu <106575414+kohei-irohasu@users.noreply.github.com>
2023-12-25 12:32:09 +0900

    #3 (#8)

--- follows/models.py
+++ follows/models.py
@@ -1,3 +1,21 @@
+import uuid
 from django.db import models
 
+from config import settings
+
 # Create your models here.
+class UserFollow(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    follower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='following',
+    )
+    followee = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='followers',
+    )
+    
+    class Meta:
+        unique_together = ['follower', 'followee']

ba9709929f9c7cc8bd93997ac9acbeece47f8862
kohei-irohasu <yktmd059@icloud.com>
2023-12-28 14:47:51 +0900

    #6

--- stories/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Story
-
-class StorySerializer(serializers.ModelSerializer):
-    """キャリアストーリー用のシリアライザ"""
-    class Meta:
-        model = Story
-        fields = ['id', 'title', 'text', 'private', 'user', 'tags']
--- tags/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Tag
-
-class TagSerializer(serializers.ModelSerializer):
-    """タグ用のシリアライザ"""
-    class Meta:
-        model = Tag
-        fields = ['id', 'name']
